c854396836a1668ef22eb77e5d811c11ca2e2b10
Yani Asari <yani.iman1@gmail.com>
2020-08-01 04:55:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,7 +32,7 @@ end
 
 def retrieve_first_element_from_array(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array [1]
+  array [0]
 end
 
 def retrieve_last_element_from_array(array)
