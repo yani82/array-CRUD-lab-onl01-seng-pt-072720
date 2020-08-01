afdda129e6b163a6e85a72da08bc79f5146af23b
Yani Asari <yani.iman1@gmail.com>
2020-08-01 03:52:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,8 @@ end
 def add_element_to_end_of_array(array, element)
   add_element_to_end_of_array = ["Yani", "Vienna", "Samir", "Noor"]
   add_element_to_end_of_array << "Milet" 
-  puts add_element_to_end_of_array.inspect
+end
+  add_element_to_end_of_array
 end
 
 def add_element_to_start_of_array(array, element)
