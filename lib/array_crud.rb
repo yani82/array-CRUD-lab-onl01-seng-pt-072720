475b611e70d5813a1af52e8599422630635fbfa2
Yani Asari <yani.iman1@gmail.com>
2020-08-01 04:28:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,13 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
- ["Yani", "Vienna", "Samir", "Noor"]
+ ["wow", "I", "am", "really", "learning"]
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array = ["Yani", "Vienna", "Samir", "Noor"]
-  add_element_to_end_of_array << "Milet" 
-  add_element_to_end_of_array.inspect
+   << 
 end
 
 def add_element_to_start_of_array(array, element)
