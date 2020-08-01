272ac39114fc3d80d86c9cafbd9683ccec89fc4f
Yani Asari <yani.iman1@gmail.com>
2020-08-01 04:33:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["wow", "I", "am", "really", "learning"]
+  ["wow", "I", "am", "really"]
 end
 
 def add_element_to_end_of_array(array, element)
