7969576834a3c7ce321eee0bfd8bff81660534ff
Yani Asari <yani.iman1@gmail.com>
2020-08-01 03:33:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   family = ["Yani", "Vienna", "Samir", "Noor"]
-  family << "Milet" 
+  << "Milet" 
 end
 
 def add_element_to_start_of_array(array, element)
