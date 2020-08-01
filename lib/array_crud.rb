e04ae8f7ef02030e1b0f036fe77208ace0007856
Yani Asari <yani.iman1@gmail.com>
2020-08-01 04:52:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,7 @@ end
 
 def remove_element_from_start_of_array(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
